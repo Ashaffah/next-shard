683707d7bf77770d107cff2e865d13ae39510285
Ashaffah <ashaffarafi@gmail.com>
2022-04-14 15:32:58 +0700

    adjustment navbar and initial product card

--- components/navbar.js
+++ components/navbar.js
@@ -28,177 +28,179 @@ class Navbar extends Component {
     } else if (window.location.pathname.includes("/category")) {
       this.setState({ active: "Category" });
     }
-    // console.log("BBBBBBBBB", window.location.pathname);
+    console.log("BBBBBBBBB", window.location.pathname);
   }
 
   render() {
     const { router } = this.props;
     const { active, menu } = this.state;
     return (
-      <nav className="bg-gray-800">
-        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-          <div className="relative flex items-center justify-between h-16">
-            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-              <button
-                type="button"
-                className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-
-                <svg
-                  className="block h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
+      <div className="relative ">
+        <nav className="fixed top-0 left-0 right-0 bg-gray-800">
+          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                <button
+                  type="button"
+                  className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                  aria-controls="mobile-menu"
+                  aria-expanded="false"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
+                  <span className="sr-only">Open main menu</span>
 
-                <svg
-                  className="hidden h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-              <nav className="text-gray-300">
-                <Link href="/">RF|DA</Link>
-              </nav>
-              <div className="hidden sm:block sm:ml-6 w-full">
-                <div className="flex space-x-4">
-                  {menu.map((menu, idx) => (
-                    <div
-                      key={idx}
-                      className={
-                        active == menu.name
-                          ? "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                      }
-                      onClick={() => {
-                        router.push(menu.route);
-                        this.setState({ active: menu.name });
-                      }}
-                    >
-                      {menu.name}
-                    </div>
-                  ))}
-                  <div className="mx-3 w-full">
-                    <div className="input-group relative flex mx-4">
-                      <input
-                        type="search"
-                        className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                        placeholder="Search"
-                        aria-label="Search"
-                        aria-describedby="button-addon2"
-                      />
-                      <button
-                        className="btn inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out flex items-center"
-                        type="button"
-                        id="button-addon2"
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+
+                  <svg
+                    className="hidden h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <nav className="text-gray-300 text-2xl">
+                  <Link href="/">RF|DA</Link>
+                </nav>
+                <div className="hidden sm:block sm:ml-6 w-full">
+                  <div className="flex space-x-4">
+                    {menu.map((menu, idx) => (
+                      <div
+                        key={idx}
+                        className={
+                          active == menu.name
+                            ? "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                        }
+                        onClick={() => {
+                          router.push(menu.route);
+                          this.setState({ active: menu.name });
+                        }}
                       >
-                        <svg
-                          aria-hidden="true"
-                          focusable="false"
-                          data-prefix="fas"
-                          data-icon="search"
-                          className="w-4"
-                          role="img"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 512 512"
+                        {menu.name}
+                      </div>
+                    ))}
+                    <div className="mx-3 w-full">
+                      <div className="input-group relative flex mx-4">
+                        <input
+                          type="search"
+                          className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          placeholder="Search"
+                          aria-label="Search"
+                          aria-describedby="button-addon2"
+                        />
+                        <button
+                          className="btn inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out flex items-center"
+                          type="button"
+                          id="button-addon2"
                         >
-                          <path
-                            fill="currentColor"
-                            d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
-                          ></path>
-                        </svg>
-                      </button>
+                          <svg
+                            aria-hidden="true"
+                            focusable="false"
+                            data-prefix="fas"
+                            data-icon="search"
+                            className="w-4"
+                            role="img"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 512 512"
+                          >
+                            <path
+                              fill="currentColor"
+                              d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
+                            ></path>
+                          </svg>
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              <button
-                type="button"
-                className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-              >
-                <span className="sr-only">View notifications</span>
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <button
+                  type="button"
+                  className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                  />
-                </svg>
-              </button>
-
-              <div className="ml-3 relative">
-                <div>
-                  <button
-                    type="button"
-                    className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                    id="user-menu-button"
-                    aria-expanded="false"
-                    aria-haspopup="true"
+                  <span className="sr-only">View notifications</span>
+                  <svg
+                    className="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
                   >
-                    <span className="sr-only">Open user menu</span>
-                    <img
-                      className="h-8 w-8 rounded-full"
-                      src="https://avatars.githubusercontent.com/u/35194643?v=4"
-                      alt=""
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
                     />
-                  </button>
+                  </svg>
+                </button>
+
+                <div className="ml-3 relative">
+                  <div>
+                    <button
+                      type="button"
+                      className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                      id="user-menu-button"
+                      aria-expanded="false"
+                      aria-haspopup="true"
+                    >
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        src="https://avatars.githubusercontent.com/u/35194643?v=4"
+                        alt=""
+                      />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="sm:hidden" id="mobile-menu">
-          <div className="px-2 pt-2 pb-3 space-y-1">
-            {menu.map((menu, idx) => (
-              <div
-                key={idx}
-                className={
-                  active == menu.name
-                    ? "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-                    : "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                }
-              >
-                {menu.name}
-              </div>
-            ))}
+          <div className="sm:hidden" id="mobile-menu">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {menu.map((menu, idx) => (
+                <div
+                  key={idx}
+                  className={
+                    active == menu.name
+                      ? "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  }
+                >
+                  {menu.name}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     );
   }
 }
--- pages/product/index.js
+++ pages/product/index.js
@@ -41,11 +41,45 @@ class Product extends Component {
     return (
       <>
         <Navbar />
-        <div>
-          {product.length > 0 &&
-            product.map((product, index) => (
-              <div key={index}>{product.title}</div>
-            ))}
+        <div className="container mx-auto mt-20 py-4 md:py-8">
+          <div className="flex flex-wrap">
+            <div className="border h-20 w-full lg:w-3/12 xl:w-3/12">
+              <div className="border m-2">filter</div>
+            </div>
+
+            <div className="border h-20 w-full lg:w-9/12 xl:w-9/12">
+              <div className="flex flex-wrap lg:-mx-2 xl:-mx-2">
+                {product.length > 0 &&
+                  product.map((product, index) => (
+                    <div
+                      key={index}
+                      className="flex justify-center w-full lg:my-2 lg:px-2 lg:w-1/4 xl:my-2 xl:px-2 xl:w-1/4"
+                    >
+                      <div className="rounded-lg shadow-lg bg-white max-w-sm">
+                        <a href="#!">
+                          <img
+                            className="rounded-t-lg"
+                            src="https://mdbootstrap.com/img/new/standard/nature/184.jpg"
+                            alt=""
+                          />
+                        </a>
+                        <div className="p-6">
+                          <h5 className="text-gray-900 text-xl font-medium mb-2">
+                            {product.title}
+                          </h5>
+                          <p className="text-gray-700 text-base mb-4">
+                            {product.price}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                {/* <div className="w-full lg:my-2 lg:px-2 lg:w-1/4 xl:my-2 xl:px-2 xl:w-1/4">
+                  <div className="border">Column</div>
+                </div> */}
+              </div>
+            </div>
+          </div>
         </div>
       </>
     );
